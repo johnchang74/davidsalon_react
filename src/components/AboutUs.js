50dd47cc17b422a8e5a58623bc76a4f69847ea04
John Chang <john.jy.chang@gmail.com>
2021-04-19 16:38:28 -0400

    update

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -19,37 +19,38 @@ class AboutUs extends Component {
                     </ol>
                 </div>
                 <div class="aveda-background">
-                <section class="gallery py-5" id="gallery">
-                    <div class="container py-md-5">
-                        <h3 class="heading text-center mb-3 mb-sm-5">About Us</h3>
-                        <div class="gallery-content">
-                            <div class="row">
-                                <div class="col-md-4 col-md-6 gal-img">
-                                    <img src="/assets/images/david_kim_business_card_v1.png" alt="aegis" class="dk_business_card" />
-                                </div>
-                                <div class="col-md-4 col-sm-6 gal-img">
-                                    <h3 class="owner">Hair Stylist David Kim</h3>
-                                    <div class="owner_bio">
-                                        <div class="about-text">
-                                            <p>Canada OCT Licensed Hairstylist,</p>
-                                            <p>CTS Colourist, Aveda Concept Salon, Aveda Ambassador,</p>
-                                            <p>owner of David Kim Salon de Beauté.</p>
-                                        </div>
-                                        <div class="badge-title">
-                                            <h5>Concept Salon</h5>
-                                            <div class="blank-space"></div>
-                                            <div class="row">
-                                                <div class="col-md-1-1 badge">
-                                                    <span class="hair-color1">hc</span>
-                                                </div>
-                                                <div class="col-md-1-1 badge">
-                                                    <span class="hair-color2">pp</span>
-                                                </div>
-                                                <div class="col-md-1-1 badge">
-                                                    <span class="hair-color3">ss</span>
-                                                </div>
-                                                <div class="col-md-1-1 badge">
-                                                    <span class="hair-color4">sr</span>
+                    <section class="gallery py-5" id="gallery">
+                        <div class="container py-md-5">
+                            <h3 class="heading text-center mb-3 mb-sm-5">About Us</h3>
+                            <div class="gallery-content">
+                                <div class="row">
+                                    <div class="col-md-4 col-md-6 gal-img">
+                                        <img src="/assets/images/david_kim_business_card_v1.png" alt="aegis" class="dk_business_card" />
+                                    </div>
+                                    <div class="col-md-4 col-sm-6 gal-img">
+                                        <h3 class="owner">Hairstylist David Kim</h3>
+                                        <div class="owner_bio">
+                                            <div class="about-text">
+                                                <p>Canada OCT Licensed Hairstylist,</p>
+                                                <p>CTS, Colourist, Aveda Concept Salon, Aveda Ambassador,</p>
+                                                <p>owner of David Kim Salon de Beauté.</p>
+                                            </div>
+                                            <div class="badge-title">
+                                                <h5>Concept Salon</h5>
+                                                <div class="blank-space"></div>
+                                                <div class="row">
+                                                    <div class="col-md-1-1 badge">
+                                                        <span class="hair-color1">hc</span>
+                                                    </div>
+                                                    <div class="col-md-1-1 badge">
+                                                        <span class="hair-color2">pp</span>
+                                                    </div>
+                                                    <div class="col-md-1-1 badge">
+                                                        <span class="hair-color3">ss</span>
+                                                    </div>
+                                                    <div class="col-md-1-1 badge">
+                                                        <span class="hair-color4">sr</span>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
@@ -57,33 +58,73 @@ class AboutUs extends Component {
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </section>
-                
-                <section class="aveda_background">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-4 youtube-clip">
-                                <h5>Love for Natural Beauty | Horst Rechelbacher | Aveda</h5>
-                                <a href="https://www.youtube.com/watch?v=u31ExlbLx-s">
-                                    <img src="https://i.ytimg.com/vi/u31ExlbLx-s/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDGJAa2SxJucsuRbEXBhUqaJElrWA" alt=" "></img>
-                                </a>
-                            </div>
-                            <div class="col-md-4 youtube-clip">
-                                <h5>Aveda Sourcing Story: Ayurvedic Roots of Turmeric and Amla</h5>
-                                    <a href="https://www.youtube.com/watch?v=OVY-TLhsnjE">
-                                        <img src="https://i.ytimg.com/vi/OVY-TLhsnjE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhyGr8xs8klyh-21xruHMt7dG2Bw" alt=" "></img>
+                    </section>
+                    <section>
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-4 youtube-clip">
+                                    <h5>Love for Natural Beauty | Horst Rechelbacher | Aveda</h5>
+                                    <a href="https://www.youtube.com/watch?v=u31ExlbLx-s">
+                                        <img src="https://i.ytimg.com/vi/u31ExlbLx-s/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDGJAa2SxJucsuRbEXBhUqaJElrWA" alt=" "></img>
                                     </a>
-                            </div>
-                            <div class="col-md-4 youtube-clip">
-                                <h5>A Conversation with Horst Rechelbacher, founder of Aveda Natural Products and Intelligent Nutrients</h5>
-                                    <a href="https://www.youtube.com/watch?v=veOlGqu2suA">
-                                        <img src="https://i.ytimg.com/vi/veOlGqu2suA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCUmV_IRqaVgPoLznepj97WGGn-yQ" alt=" "></img>
-                                    </a>
+                                </div>
+                                <div class="col-md-4 youtube-clip">
+                                    <h5>Aveda Sourcing Story: Ayurvedic Roots of Turmeric and Amla</h5>
+                                        <a href="https://www.youtube.com/watch?v=OVY-TLhsnjE">
+                                            <img src="https://i.ytimg.com/vi/OVY-TLhsnjE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAhyGr8xs8klyh-21xruHMt7dG2Bw" alt=" "></img>
+                                        </a>
+                                </div>
+                                <div class="col-md-4 youtube-clip">
+                                    <h5>A Conversation with Horst Rechelbacher, founder of Aveda Natural Products and Intelligent Nutrients</h5>
+                                        <a href="https://www.youtube.com/watch?v=veOlGqu2suA">
+                                            <img src="https://i.ytimg.com/vi/veOlGqu2suA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCUmV_IRqaVgPoLznepj97WGGn-yQ" alt=" "></img>
+                                        </a>
+                                </div>
+                                <div class="col-md-4 youtube-clip">
+                                    <h5>Aveda Concept Colour at David Kim Salon de Beauté</h5>
+                                        <a href="https://www.youtube.com/watch?v=bWQxn2HxFoI">
+                                            <img src="https://i.ytimg.com/vi/bWQxn2HxFoI/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCsTyOdz4MFDZ9bdvQ4Jzubtm8qtQ" alt=" "></img>
+                                        </a>
+                                </div>
+                                <div class="col-md-4 youtube-clip">
+                                    <h5>Aveda Concept Colour at David Kim Salon de Beauté</h5>
+                                        <a href="https://www.youtube.com/watch?v=vQZbcvc9Of4">
+                                            <img src="https://i.ytimg.com/vi/vQZbcvc9Of4/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB89pLtVRtjUpebrHAPRTSrlMSNmg" alt=" "></img>
+                                        </a>
+                                </div>
+                                <div class="col-md-4 youtube-clip">
+                                    <h5>Aveda damage remedy  Chemical Damage Repair  David Kim Salon de Beauté</h5>
+                                        <a href="https://www.youtube.com/watch?v=KHCvHukfpp4">
+                                            <img src="https://i.ytimg.com/vi/KHCvHukfpp4/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB_lh3bzPlDU678m4udrh26d_68Mg" alt=" "></img>
+                                        </a>
+                                </div>
+                                <div class="col-md-4 youtube-clip">
+                                    <h5>Aveda Concept Colour at David Kim Salon de Beauté</h5>
+                                        <a href="https://www.youtube.com/watch?v=V1ci_jNe4_A">
+                                            <img src="https://i.ytimg.com/vi/V1ci_jNe4_A/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCd551yefuaNNYzZ5QMNUrcanaOWg" alt=" "></img>
+                                        </a>
+                                </div>
+                                <div class="col-md-4 youtube-clip">
+                                    <h5>Botanical Treatment with David Kim Salon de Beauté</h5>
+                                        <a href="https://www.youtube.com/watch?v=fuslk89S6RU">
+                                            <img src="https://i.ytimg.com/vi/fuslk89S6RU/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBkj_DezJqEcvumM2hWdYkpNipp0A" alt=" "></img>
+                                        </a>
+                                </div>
+                                <div class="col-md-4 youtube-clip">
+                                    <h5>Curbside notification Aveda products</h5>
+                                        <a href="https://www.youtube.com/watch?v=G-1rc3z2Pmc">
+                                            <img src="https://i.ytimg.com/vi/G-1rc3z2Pmc/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD0CcHEwnENcc6FTYdK3Kg1yPo38Q" alt=" "></img>
+                                        </a>
+                                </div>
+                                <div class="col-md-4 youtube-clip">
+                                    <h5>Aveda Concept Colour at David Kim Salon de Beauté</h5>
+                                        <a href="https://www.youtube.com/watch?v=BZ2pCfwkB14">
+                                            <img src="https://i.ytimg.com/vi/BZ2pCfwkB14/hqdefault.jpg?sqp=-oaymwEcCPYBEIoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDF3sVK3jqLe-iy_DJX5Pb2NnIkkQ" alt=" "></img>
+                                        </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </section>
+                    </section>
                 </div>
             </div>
         )
--- src/components/partial/Topbar.js
+++ src/components/partial/Topbar.js
@@ -8,17 +8,6 @@ class Topbar extends Component{
 			<div class="topbar_row">
 				<div class="col-md-12">
 					<Navbar className="color-nav" variant="dark" expand="lg" sticky="top">
-						<Navbar.Toggle aria-controls="basic-navbar-nav"/>
-						<Navbar.Collapse id="basic-navbar-nav">
-							<Nav className="mr-auto">
-								<Nav.Link href="/">Home</Nav.Link>
-								<Nav.Link href="/aboutus">About Us</Nav.Link>
-								<Nav.Link href="/services">Services</Nav.Link>
-								<Nav.Link href="https://www.aveda.ca/salon/dk-salon-de-beaute">Shop Online</Nav.Link>
-								<Nav.Link href="/products">Products</Nav.Link>
-								<Nav.Link href="/contact">Book Appointment</Nav.Link>
-							</Nav>
-						</Navbar.Collapse>
 						<Navbar.Brand href="/" className="log-nav">
 							<img
 								src="/assets/images/david_salon_logo1_v1.jpg"
@@ -32,13 +21,24 @@ class Topbar extends Component{
 								height="20px"
 								className="d-inline-block align-top"
 								alt=" "/>
-							<img
+							{/* <img
 								src="/assets/images/aveda_concept_salon.png"
 								width="90px"
 								height="50px"
 								className="d-inline-block align-top"
-								alt=" "/>
+								alt=" "/> */}
 						</Navbar.Brand>
+						<Navbar.Toggle aria-controls="basic-navbar-nav"/>
+						<Navbar.Collapse id="basic-navbar-nav">
+							<Nav className="ml-auto">
+								<Nav.Link href="/">Home</Nav.Link>
+								<Nav.Link href="/aboutus">About Us</Nav.Link>
+								<Nav.Link href="/services">Services</Nav.Link>
+								<Nav.Link href="https://www.aveda.ca/salon/dk-salon-de-beaute">Shop Online</Nav.Link>
+								<Nav.Link href="/products">Products</Nav.Link>
+								<Nav.Link href="/contact">Book Appointment</Nav.Link>
+							</Nav>
+						</Navbar.Collapse>
 					</Navbar>
 					<br/>
 				</div>
