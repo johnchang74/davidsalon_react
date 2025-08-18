94bcedb26f0f6b4dcc45f0d72e9c1de86f326bf1
John Chang <johnchang@Johns-MacBook-Pro.local>
2025-08-17 22:02:28 -0400

    Reverted the pdf invoice issue

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -179,13 +179,6 @@ class Contact extends Component {
                   No walk-in please. All of our services are provided by
                   appointment ONLY.
                 </p>
-                <div class="blank-space"></div>
-                {/* <div class="wish-block">
-                                    <p class="wish-note">For <a href="https://www.wish.com/local">Wish</a> pickup customers, 
-                                    please pick up your package only from Monday to Wednesday between 11AM and 2PM.</p>
-                                </div> */}
-                <div class="blank-space"></div>
-                <p>Please comply with COVID-19 rules for any visit.</p>
               </h4>
             </div>
             <div class="contact-w3pvt-form mt-5">
