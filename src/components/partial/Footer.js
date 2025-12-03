2c3df7a3514c5cc8d864b86758bc24baeef8bcd0
John Chang <johnchang@Johns-MacBook-Pro.local>
2025-12-03 18:22:40 -0500

    Rmoved phone and email for vaughan

--- src/components/partial/Footer.js
+++ src/components/partial/Footer.js
@@ -141,12 +141,12 @@ class Footer extends Component {
                   </div>
                   <div className="mt-5">
                     <span className="store-location">Vaughan location:</span>
-                    <div className="last-w3ls-contact">
+                    {/* <div className="last-w3ls-contact">
                       <p>+1 905 738 8976</p>
                     </div>
                     <div className="last-w3ls-contact">
                       <p>vaughan@fiorio.com</p>
-                    </div>
+                    </div> */}
                     <div className="last-w3ls-contact">
                       <p>
                         #104A-1 Bass Pro Mills Drive
